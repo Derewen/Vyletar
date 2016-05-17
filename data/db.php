586b90993e4effc0c58ffefdc231c86a3094b171
Derewen <Derewen@gmail.com>
2016-05-18 01:25:31 +0200

    Delete db.php

--- data/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-//pripojeni do db na serveru eso.vse.cz
-$db = new PDO('mysql:host=127.0.0.1;dbname=USERNAME;charset=utf8', 'USERNAME', 'heslo');
-//vyhazuje vyjimky v pripade neplatneho SQL vyrazu
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
-?>
-
