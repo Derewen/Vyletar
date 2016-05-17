9e2e9361bd28dbc9d6793d28afb35a31ef3fac1b
Derewen <Derewen@gmail.com>
2016-05-18 01:17:32 +0200

    Update db.php

--- data/db.php
+++ data/db.php
@@ -1,6 +1,6 @@
 <?php
 //pripojeni do db na serveru eso.vse.cz
-$db = new PDO('mysql:host=127.0.0.1;dbname=sucs00;charset=utf8', 'sucs00', 'GufZte8C0IpBaNofr2');
+$db = new PDO('mysql:host=127.0.0.1;dbname=USERNAME;charset=utf8', 'USERNAME', 'heslo');
 //vyhazuje vyjimky v pripade neplatneho SQL vyrazu
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
 ?>
